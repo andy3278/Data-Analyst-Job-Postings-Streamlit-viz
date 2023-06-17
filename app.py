cf126b6f0758a9a3c99cf3d56ebde0b6bee40624
Andy Cheung <77382250+andy3278@users.noreply.github.com>
2023-06-17 17:42:31 +0800

    use google cloud api

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 import streamlit as st
 import pandas as pd
+from google.oauth2 import service_account
+from gsheetsdb import connect
 
+import streamlit as st
+from st_files_connection import FilesConnection
 
-df = pd.read_csv('data/gsearch_jobs.csv')
-st.write(df.head(15))
+# Create connection object and retrieve file contents.
+# Specify input format is a csv and to cache the result for 600 seconds.
+conn = st.experimental_connection('gcs', type=FilesConnection)
+df = conn.read("bucket-fro-streamlit/gsearch_jobs.csv", input_format="csv", ttl=600)
+
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.title} \n {row.company_name}:")
